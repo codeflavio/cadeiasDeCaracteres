ef1dca887a5c4eaf84055106abe9f1015da6df9c
codeflavio <flaviopvieira@hotmail.com>
2020-12-31 09:14:22 -0300

    projeto

---
+++ cadeias de caracteres/Cadeia de caracteres/Cadeia de caracteres/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cadeia_de_caracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstFriend  = "Maria";
+            string secondFriend = "João";
+            Console.WriteLine($"O nome {firstFriend} has {firstFriend.Length}letters.");
+            Console.WriteLine($"O nome {secondFriend} has {secondFriend.Length}letters.");
+
+
+
+                
+        }
+    }
+}
---
+++ cadeias de caracteres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cadeias_de_caracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
